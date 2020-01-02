676b2a94f276e6bf1a759e8334b87a6f705a8f64
Aleksander <olek130@gmail.com>
2020-01-02 02:40:54 +0100

    Removed stray debug message :)

--- src/routers/user.js
+++ src/routers/user.js
@@ -19,7 +19,6 @@ router.post( "/users", async ( req, res ) => {
 } );
 
 router.post( "/users/login", async ( req, res ) => {
-	console.log( "dupa" );
 	try {
 		const user = await User.findByCredentials( req.body.email, req.body.password );
 		const token = await user.generateAuthToken();
